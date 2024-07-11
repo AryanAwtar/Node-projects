96fec49843e092d779b79962f9ef86bfe838c059
AryanAwtar <aryanshankar302@outlook.com>
2024-07-11 17:21:20 +0530

    Connect the frontend and backend

---
+++ Jokes-APP/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import axios from "axios";
+
+
+const app = express();
+const port = 3000;
+const API_URL= "https://v2.jokeapi.dev/joke"
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+    });
+
+app.get("/", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/Any");
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
